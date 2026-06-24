31008a0e8e25ad0fc7eb73adb6230fb6247e86d0
subhajitdeo <subhajitdeoghoria5@gmail.com>
2026-06-24 17:12:23 +0530

    Update content.js

--- content.js
+++ content.js
@@ -1,44 +1,7 @@
-console.clear();
+const script = document.createElement("script");
 
-console.log("================================");
-console.log("STEP 2 - FIND TV WIDGET");
-console.log("================================");
+script.src = chrome.runtime.getURL("inject.js");
 
-console.log("TradingView =", TradingView);
+document.documentElement.appendChild(script);
 
-console.log("Searching window...");
-
-for (const key of Object.getOwnPropertyNames(window)) {
-
-    try {
-
-        const value = window[key];
-
-        if (!value) continue;
-
-        // look for activeChart()
-        if (typeof value.activeChart === "function") {
-
-            console.log("FOUND activeChart ->", key, value);
-
-        }
-
-        // look for widget
-        if (
-            typeof value === "object" &&
-            value !== null &&
-            value._id &&
-            value._ready !== undefined
-        ) {
-
-            console.log("Possible Widget ->", key, value);
-
-        }
-
-    } catch (e) {}
-
-}
-
-console.log("================================");
-console.log("DONE");
-console.log("================================");
+script.onload = () => script.remove();
